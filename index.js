c1b17cddecbf367d59ebd8761416f3a29b138387
E H <erichou24@gmail.com>
2021-03-22 11:38:26 -0400

    feat: wrapped callbacks in promise constructors within functions

--- index.js
+++ index.js
@@ -9,30 +9,65 @@ switch (myArgs[0]) {
     let now = new Date()
     let fp = './times'
     console.log(`You did it at ${now}`)
-    fs.readFile(fp, 'utf8', (err, data) => {
-      // Everything under this only executes after readFile is finished
-      if (err) {
-        console.error(err)
-        return
-      }
-      // If this is the first time then the textfile is empty
+    rfPromise(fp, 'utf8').then(data => {
       if (data === '') {
         console.log(`This is the first time you've done it!`)
+        return afPromise(fp, now.toString() + '\n')
       } else {
         let times = data.trim().split('\n')
         let lastTime = times[times.length - 1]
         console.log(`The last time you did it was at: ${lastTime}`)
+        return afPromise(fp, now.toString() + '\n')
       }
-      // We dont want to write to the file while we're reading it 
-      // We call appendFile in the callback of readFile
-      fs.appendFile(fp, now.toString() + '\n', err => {
-        if(err) {
-          console.error(err)
-          return
-        }
-      })
+    }).then().catch(err => {
+      console.error(err)
     })
     break
   default:
     console.log('Sorry, try another input')
 }
+
+function rfPromise(fp, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fp, encoding, (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(data)
+    })
+  })
+}
+
+function afPromise(fp, timestampString) {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(fp, timestampString, err => {
+      if (err) {
+        return reject(err)
+      }
+    })
+  })
+}
+
+// fs.readFile(fp, 'utf8', (err, data) => {
+//   // Everything under this only executes after readFile is finished
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+//   // If this is the first time then the textfile is empty
+//   if (data === '') {
+//     console.log(`This is the first time you've done it!`)
+//   } else {
+//     let times = data.trim().split('\n')
+//     let lastTime = times[times.length - 1]
+//     console.log(`The last time you did it was at: ${lastTime}`)
+//   }
+//   // We dont want to write to the file while we're reading it 
+//   // We call appendFile in the callback of readFile
+//   fs.appendFile(fp, now.toString() + '\n', err => {
+//     if(err) {
+//       console.error(err)
+//       return
+//     }
+//   })
+// })
