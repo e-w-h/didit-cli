8e72ddcba46a13ce471a1b30127f29b10708cf13
E H <erichou24@gmail.com>
2021-03-22 04:45:14 -0400

    restructure and comment

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ switch (myArgs[0]) {
     let fp = './times'
     console.log(`You did it at ${now}`)
     fs.readFile(fp, 'utf8', (err, data) => {
+      // Everything under this only executes after readFile is finished
       if (err) {
         console.error(err)
         return
       }
-      console.log(`This is the data:\n ${data}`)
+      // If this is the first time then the textfile is empty
       if (data === '') {
         console.log(`This is the first time you've done it!`)
+      } else {
+        console.log(`This is the data:\n ${data}`)
       }
+      // We dont want to write to the file while we're reading it 
+      // We call appendFile in the callback of readFile
       fs.appendFile(fp, now.toString() + '\n', err => {
         if(err) {
           console.error(err)
